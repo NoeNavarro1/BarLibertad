9be327efc0296c08caab945aff80cad65dae52c3
Lorelei <pamelaorte05@gmail.com>
2024-10-21 10:37:06 -0600

    Home

--- barlib/apps/home/urls.py
+++ barlib/apps/home/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from . import views
+from django.conf.urls.static import static
+from django.conf import settings
 
 app_name = 'home'
 urlpatterns = [
     path('', views.home, name='home'),
-]
+    path('registro/', views.registro_producto_view, name='registro_producto'),
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
