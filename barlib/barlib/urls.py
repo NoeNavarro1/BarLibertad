10b0d4d094f30a8a88a833d311efe90d8b95ac83
NoeNavarro1 <noenavarroacevedo92@gmail.com>
2024-10-22 10:39:44 -0600

    Estilizando

--- barlib/barlib/urls.py
+++ barlib/barlib/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import path, include #Añadimos include para nuestras urls
 
 urlpatterns = [
+    path('', include('apps.login.urls')),
     path('admin/', admin.site.urls),
     path('principal/', include('apps.principal.urls')),
-    path('home/', include('apps.home.urls')),
     ]
